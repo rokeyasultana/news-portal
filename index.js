38dde14ae0307273ba653636fd16747e4c58865a
jakiasultana2022 <rokeyasbd@gmail.com>
2022-09-03 23:24:59 +0600

    category

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ spinner.style.display = 'none';
 
 
 // api url
-const api_url = 
-      "https://openapi.programming-hero.com/api/news/categories";
-  
+// const api_url = 
+//       "https://openapi.programming-hero.com/api/news/categories";
+
+
+ fetch('https://openapi.programming-hero.com/api/news/categories').then((data)=>{
+    return data.json();
+ }).then((completeData)=>{
+    // console.log(completeData.data.news_category[2].category_name);
+
+    let data1 ='';
+    completeData.data.news_category.map((values)=>{
+   data1 += `<div class="">
+   <ul  class="menu text-black p-2 rounded-box">
+   <li><a>${values.category_name}</a></li>
+   
+</ul>
+ </div> `
+
+    });
+
+    document.getElementById('category').innerHTML = data1
+    
+
+ }).catch((err)=>{
+    console.log(err);
+ })
+   
+
+
