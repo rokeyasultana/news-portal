f1b0dbec11c91d820c370f771c4b667d53011033
jakiasultana2022 <rokeyasbd@gmail.com>
2022-09-03 20:29:38 +0600

    question-2

---
+++ index.js
@@ -0,0 +1,10 @@
+// Spinner
+
+const spinner = document.getElementById('spinner');
+spinner.style.display = 'none';
+
+
+// api url
+const api_url = 
+      "https://openapi.programming-hero.com/api/news/categories";
+  
